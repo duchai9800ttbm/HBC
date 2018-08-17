f1920aa6c1727f248c4e7683695cf2961b264b6c
DangQuyen <quyendang13dt1@gmail.com>
2018-08-17 18:49:50 +0700

    up

--- src/app/layout/package/package-detail/package-detail-info/edit/edit.component.ts
+++ src/app/layout/package/package-detail/package-detail-info/edit/edit.component.ts
@@ -114,7 +114,7 @@ export class EditComponent implements OnInit {
             magnitude: [this.package.magnitude],
             constructionTypeId: [this.package.projectType && this.package.projectType.id, Validators.required],
             constructionCategoryId: [this.package.mainBuildingCategory && this.package.mainBuildingCategory.id, Validators.required],
-            hbcRole: [this.package.hbcRole.id],
+            hbcRole: [this.package.hbcRole ? this.package.hbcRole.id : ''],
             documentLink: [this.package.documentLink],
             chairEmployeeId: [this.package.chairEmployee && this.package.chairEmployee.text],
             bidStatusId: [this.package.status && this.package.status.id, Validators.required],
