0c58f921e6195c7983ae3ae93571d2caab046363
Huy Nguyen Nhat <nnhuy@sdc.ud.edu.vn>
2018-08-28 15:53:40 +0700

    - update code phan quyen goi thau

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api_endpoint: 'http://apicrm.vinhmy.com:6868/vinmus-crm/',
+  api_endpoint: 'http://115.79.35.119:9004/api/hbc/',
   api_worldfone: 'https://apps.worldfone.vn/externalcrm/'
   // api_endpoint: 'http://115.79.35.119:7878/vinmus-crm/',
   // api_worldfone: 'https://apps.worldfone.vn/externalcrm/'
