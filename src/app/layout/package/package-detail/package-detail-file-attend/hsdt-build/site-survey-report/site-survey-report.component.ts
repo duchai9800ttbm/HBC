9f3be0a5c319dd1b22e48f67f5c9b6de5c18530e
Khanh Hoang <hoangduykhanh21@gmail.com>
2018-08-30 00:23:58 +0700

    up

--- src/app/layout/package/package-detail/package-detail-file-attend/hsdt-build/site-survey-report/site-survey-report.component.ts
+++ src/app/layout/package/package-detail/package-detail-file-attend/hsdt-build/site-survey-report/site-survey-report.component.ts
@@ -1,26 +1,68 @@
-import { Component, OnInit, AfterViewChecked, AfterViewInit } from '@angular/core';
-import { link } from 'fs';
-
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
+import { DATETIME_PICKER_CONFIG } from '../../../../../../shared/configs/datepicker.config';
 @Component({
   selector: 'app-site-survey-report',
   templateUrl: './site-survey-report.component.html',
   styleUrls: ['./site-survey-report.component.scss']
 })
 export class SiteSurveyReportComponent implements OnInit, AfterViewInit {
-  constructor() {
+  isData;
+  datePickerConfig = DATETIME_PICKER_CONFIG;
+  fakeData = [
+    {
+      stt: 0,
+      ttl: 'Tài liệu nghìn đô',
+      lpv: 4,
+      maker: 'Khanh Hoang',
+      date: '30/08/2018'
+    },
+    {
+      stt: 1,
+      ttl: 'Tài liệu tỉ đô',
+      lpv: 2,
+      maker: 'Khanh Hoang 1',
+      date: '30/08/2018'
+    }
+  ];
+
+  constructor(
+    private router: Router,
+  ) {
+    router.events.subscribe((event: Event) => {
+
+      if (event instanceof NavigationStart) {
+        // Show loading indicator
+      }
+
+      if (event instanceof NavigationEnd) {
+        // Hide loading indicator
+      }
+
+      if (event instanceof NavigationError) {
+        // Hide loading indicator
+        // Present error to user
+        console.log(event.error);
+      }
+    });
   }
 
   ngOnInit() {
+    (this.fakeData.length) ? this.isData = true : this.isData = false;
   }
   ngAfterViewInit() {
     // Đang test nên để ở đây ^^
     const linkView = location.href.match(/([^\/]*)\/*$/)[1];
     if (linkView === 'sitereport') {
       const elem = Array.from(document.querySelectorAll('#contentDisplay, #toggle-menu-child, #header-table-child'));
-      elem.forEach(e => e.parentNode.removeChild(e));
+      // elem.forEach(e => e.parentNode.removeChild(e));
+      elem.forEach(e => (<HTMLElement>e).style.display = 'none');
+      console.log(elem);
     } else {
       const elem = Array.from(document.querySelectorAll('#header-table, #toggle-menu, #template0, #template1'));
-      elem.forEach(x => x.parentNode.removeChild(x));
+      // elem.forEach(x => x.parentNode.removeChild(x));
+      elem.forEach(e => (<HTMLElement>e).style.display = 'none');
+      console.log(elem);
     }
   }
 }
---
+++ src/app/shared/services/liveform-build.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
+
+@Injectable()
+export class LiveformBuildService {
+
+  constructor() { }
+}
