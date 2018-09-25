055376938181cf0e6bf72813eece0d7e79b375a9
Huy Nguyen Nhat <nnhuy@sdc.ud.edu.vn>
2018-09-25 10:01:49 +0700

    - xóa các module không dùng do clone code

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from './layout.component';
-import { ChangeListComponent } from './change-list/change-list.component';
 import { NotificationListComponent } from './notification-list/notification-list.component';
 
 const routes: Routes = [
@@ -11,21 +10,21 @@ const routes: Routes = [
         children: [
             { path: '', redirectTo: 'package' },
             { path: 'package', loadChildren: './package/package.module#PackageModule' },
-            { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
+            // { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
             { path: 'management-user', loadChildren: './management-user/management-user.module#ManagementUserModule' },
             // { path: 'branch', loadChildren: './branch-selection/branch-selection.module#BranchSelectionModule' },
-            { path: 'prospect', loadChildren: './prospect/prospect.module#ProspectModule' },
+            // { path: 'prospect', loadChildren: './prospect/prospect.module#ProspectModule' },
             { path: 'user', loadChildren: './user/user.module#UserModule' },
-            { path: 'customer', loadChildren: './customer/customer.module#CustomerModule' },
-            { path: 'contact', loadChildren: './contact/contact.module#ContactModule' },
-            { path: 'opportunity', loadChildren: './opportunity/opportunity.module#OpportunityModule' },
-            { path: 'campaign', loadChildren: './campaign/campaign.module#CampaignModule' },
-            { path: 'activity', loadChildren: './activity/activity.module#ActivityModule' },
-            { path: 'change-list', component: ChangeListComponent },
+            // { path: 'customer', loadChildren: './customer/customer.module#CustomerModule' },
+            // { path: 'contact', loadChildren: './contact/contact.module#ContactModule' },
+            // { path: 'opportunity', loadChildren: './opportunity/opportunity.module#OpportunityModule' },
+            // { path: 'campaign', loadChildren: './campaign/campaign.module#CampaignModule' },
+            // { path: 'activity', loadChildren: './activity/activity.module#ActivityModule' },
+            // { path: 'change-list', component: ChangeListComponent },
             { path: 'notification-list', component: NotificationListComponent },
             { path: 'blank-page', loadChildren: './blank-page/blank-page.module#BlankPageModule' },
-            { path: 'revenue', loadChildren: './revenue-detail/revenue-detail.module#RevenueDetailModule' },
-            { path: 'delivery', loadChildren: './delivery/delivery.module#DeliveryModule' },
+            // { path: 'revenue', loadChildren: './revenue-detail/revenue-detail.module#RevenueDetailModule' },
+            // { path: 'delivery', loadChildren: './delivery/delivery.module#DeliveryModule' },
             { path: 'settings', loadChildren: './settings/settings.module#SettingsModule'}
         ]
     }
