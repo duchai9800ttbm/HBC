86491ae0b772429f0f98dbdbd4d58208a2a57a00
DangQuyen <quyen.dang@bys.com.vn>
2018-10-02 11:50:59 +0700

    - fix notificaton & message

--- src/app/layout/package/package-detail/package-detail-file-attend/information-deployment/information-deployment.component.ts
+++ src/app/layout/package/package-detail/package-detail-file-attend/information-deployment/information-deployment.component.ts
@@ -398,7 +398,7 @@ export class InformationDeploymentComponent implements OnInit {
         this.alertService.error('Gửi phân công tiến độ thất bại!');
       });
     } else {
-      this.confirmService.missAction('Bảng phân công tiến độ chưa được ký duyệt',
+      this.confirmService.missAction('Bảng phân công tiến độ chưa được xác nhận ký tại Người tạo và người duyệt',
         `/package/detail/${this.bidOpportunityId}/attend/infomation-deployment/edit`);
     }
   }
