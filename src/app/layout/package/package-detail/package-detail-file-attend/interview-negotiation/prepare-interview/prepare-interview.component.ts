eed9589559d06bad2460e81bd2fa5337101aedae
DangQuyen <quyen.dang@bys.com.vn>
2018-10-02 16:09:01 +0700

    - fix interview

--- src/app/layout/package/package-detail/package-detail-file-attend/interview-negotiation/prepare-interview/prepare-interview.component.ts
+++ src/app/layout/package/package-detail/package-detail-file-attend/interview-negotiation/prepare-interview/prepare-interview.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '../../../../../../../../node_modules/@angular/router';
+import { PackageDetailComponent } from '../../../package-detail.component';
 
 @Component({
   selector: 'app-prepare-interview',
   templateUrl: './prepare-interview.component.html',
-  styleUrls: ['./prepare-interview.component.scss']
+  styleUrls: ['./prepare-interview.component.scss'],
 })
 export class PrepareInterviewComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+  packageId = +PackageDetailComponent.packageId;
   ngOnInit() {
+  }
+
+  routerLink(link: string) {
+    this.router.navigate([`/package/detail/${this.packageId}/attend/interview-negotiation/${link}`]);
   }
 
 }
