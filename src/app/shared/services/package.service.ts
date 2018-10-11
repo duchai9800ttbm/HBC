24b43d5bdc79fd8b32a5f88c0f2082837f558217
Khanh Hoang <hoangduykhanh21@gmail.com>
2018-10-11 16:48:33 +0700

    Fixed Đánh giá dự án_Create_Edit_Info Form Package

--- src/app/shared/services/package.service.ts
+++ src/app/shared/services/package.service.ts
@@ -20,7 +20,6 @@ import { BidPermissionGroupResponsive } from '../models/api-response/setting/bid
 import * as FileSaver from 'file-saver';
 import { TenderPreparationPlanningRequest } from '../models/api-request/package/tender-preparation-planning-request';
 import { ProposedTenderParticipationHistory } from '../models/api-response/package/proposed-tender-participation-history.model';
-import { EvaluationModel } from '../models/package/evaluation.model';
 
 @Injectable()
 export class PackageService {
@@ -204,7 +203,7 @@ export class PackageService {
     ) { }
 
     // Get Evaluation List
-    getEvaluationValue(): Observable<EvaluationModel> {
+    getEvaluationValue() {
         const url = `data/evaluation`;
         return this.apiService.get(url).map(res => res.result);
     }
