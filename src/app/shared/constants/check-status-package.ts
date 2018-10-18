9c02a7882b23d5f99a5b5705a4e84a961f7774da
DangQuyen <quyen.dang@bys.com.vn>
2018-10-18 17:51:37 +0700

    - fix HostListener gọi API

---
+++ src/app/shared/constants/check-status-package.ts
@@ -0,0 +1,168 @@
+export const CheckStatusPackage = {
+    CanLapDeNghiDuThau: {
+        text: 'CanLapDeNghiDuThau',
+        stage: 'HSDT',
+        id: 1,
+    },
+    ChoDuyetDeNghiDuThau: {
+        text: 'ChoDuyetDeNghiDuThau',
+        stage: 'HSDT',
+        id: 2,
+    },
+    ThamGiaDuThau: {
+        text: 'ThamGiaDuThau',
+        stage: 'HSDT',
+        id: 3,
+    },
+    DaTuChoi: {
+        text: 'DaTuChoi',
+        stage: 'HSDT',
+        id: 4,
+    },
+    DaThongBaoTrienKhai: {
+        text: 'DaThongBaoTrienKhai',
+        stage: 'HSDT',
+        id: 5,
+    },
+    DaXacNhanPhanCong: {
+        text: 'DaXacNhanPhanCong',
+        stage: 'HSDT',
+        id: 5,
+    },
+    DaGuiPhanCongTienDo: {
+        text: 'DaGuiPhanCongTienDo',
+        stage: 'HSDT',
+        id: 6,
+    },
+    DangLapHSDT: {
+        text: 'DangLapHSDT',
+        stage: 'HSDT',
+        id: 7,
+    },
+    CanLapTrinhDuyetGia: {
+        text: 'CanLapTrinhDuyetGia',
+        stage: 'HSDT',
+        id: 8,
+    },
+    CanDieuChinhTrinhDuyetGia: {
+        text: 'CanDieuChinhTrinhDuyetGia',
+        stage: 'HSDT',
+        id: 9,
+    },
+    DaGuiDuyetTrinhDuyetGia: {
+        text: 'DaGuiDuyetTrinhDuyetGia',
+        stage: 'HSDT',
+        id: 10,
+    },
+    DaDuyetTrinhDuyetGia: {
+        text: 'DaDuyetTrinhDuyetGia',
+        stage: 'HSDT',
+        id: 11,
+    },
+    ChotHoSo: {
+        text: 'ChotHoSo',
+        stage: 'HSDT',
+        id: 12,
+    },
+    ChoDuyet: {
+        text: 'ChoDuyet',
+        stage: 'HSDT',
+        id: 13,
+    },
+    DaKy: {
+        text: 'DaKy',
+        stage: 'HSDT',
+        id: 14,
+    },
+    DaNopHSDT: {
+        text: 'DaNopHSDT',
+        stage: 'HSDT',
+        id: 15,
+    },
+    DaNhanLoiMoi: {
+        text: 'DaNhanLoiMoi',
+        stage: 'HSDT',
+        id: 16,
+    },
+    ChuanBiPhongVan: {
+        text: 'ChuanBiPhongVan',
+        stage: 'HSDT',
+        id: 17,
+    },
+    DaChotCongTacChuanBiPhongVan: {
+        text: 'DaChotCongTacChuanBiPhongVan',
+        stage: 'HSDT',
+        id: 18,
+    },
+    DaPhongVan: {
+        text: 'DaPhongVan',
+        stage: 'HSDT',
+        id: 19,
+    },
+    ChoKetQuaDuThau: {
+        text: 'ChoKetQuaDuThau',
+        stage: 'KQDT',
+        id: 20,
+    },
+    TratThau: {
+        text: 'TratThau',
+        stage: 'KQDT',
+        id: 21,
+    },
+    HuyThau: {
+        text: 'HuyThau',
+        stage: 'KQDT',
+        id: 22,
+    },
+    DaThongBaoCacBenLienQuan: {
+        text: 'DaThongBaoCacBenLienQuan',
+        stage: 'KQDT',
+        id: 23,
+    },
+    CoBaiHocKinhNghiem: {
+        text: 'CoBaiHocKinhNghiem',
+        stage: 'KQDT',
+        id: 24,
+    },
+    TrungThau: {
+        text: 'TrungThau',
+        stage: 'KQDT',
+        id: 25,
+    },
+    DaPhanHoiDenPHopDong: {
+        text: 'DaPhanHoiDenPHopDong',
+        stage: 'KQDT',
+        id: 26,
+    },
+    DaKyKetHopDong: {
+        text: 'DaKyKetHopDong',
+        stage: 'KQDT',
+        id: 27,
+    },
+    DaChuyenGiaoTaiLieu: {
+        text: 'DaChuyenGiaoTaiLieu',
+        stage: 'KQDT',
+        id: 28,
+    },
+    ChuaNhanTaiLieu: {
+        text: 'ChuaNhanTaiLieu',
+        stage: 'KQDT',
+        id: 29,
+    },
+    DaNhanTaiLieu: {
+        text: 'DaNhanTaiLieu',
+        stage: 'KQDT',
+        id: 30,
+    },
+    DaThongBaoHopKickOff: {
+        text: 'DaThongBaoHopKickOff',
+        stage: 'KQDT',
+        id: 31,
+    },
+    ChuaChuyenGiaoTaiLieu: {
+        text: 'ChuaChuyenGiaoTaiLieu',
+        stage: 'KQDT',
+        id: 32,
+    },
+};
+
