2bbc248e7ec760c57a290aa96b4ff95dc3ed8d54
DangQuyen <quyen.dang@bys.com.vn>
2018-11-15 23:31:10 +0700

    - fix đổi check value

--- src/app/layout/package/package-detail/package-detail-file-attend/need-create-tender/need-create-tender-form/need-create-tender-form-decision-board/need-create-tender-form-decision-board.component.ts
+++ src/app/layout/package/package-detail/package-detail-file-attend/need-create-tender/need-create-tender-form/need-create-tender-form-decision-board/need-create-tender-form-decision-board.component.ts
@@ -38,7 +38,7 @@ export class NeedCreateTenderFormDecisionBoardComponent implements OnInit {
     ngOnInit() {
         this.getPackageInfo();
         this.isDirector = (this.sessionService.currentUserInfo && this.sessionService.currentUserInfo.department
-            && this.sessionService.currentUserInfo.department.id === '42');
+            && this.sessionService.currentUserInfo.department.text === 'BAN TỔNG GIÁM ĐỐC');
         this.routerAction = this.packageService.routerAction;
         this.packageService.routerAction$.subscribe(router => {
             this.routerAction = router;
