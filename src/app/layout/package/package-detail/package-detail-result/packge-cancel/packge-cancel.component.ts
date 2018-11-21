6369d96bbc54b65872eee1393864bed87eb0e745
DangQuyen <quyen.dang@bys.com.vn>
2018-11-21 10:08:26 +0700

    - fix CSS and UI Cancel-result

--- src/app/layout/package/package-detail/package-detail-result/packge-cancel/packge-cancel.component.ts
+++ src/app/layout/package/package-detail/package-detail-result/packge-cancel/packge-cancel.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router,ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { PackageDetailComponent } from '../../package-detail.component';
 import { ConfirmationService, AlertService } from '../../../../../shared/services';
 import { slideToBottom, slideToTop } from '../../../../../router.animations';
+import { DetailResultPackageService } from '../../../../../shared/services/detail-result-package.service';
 @Component({
   selector: 'app-packge-cancel',
   templateUrl: './packge-cancel.component.html',
@@ -12,11 +13,40 @@ import { slideToBottom, slideToTop } from '../../../../../router.animations';
 export class PackgeCancelComponent implements OnInit {
   currentPackageId: number;
   constructor(
-    private alertService: AlertService
+    private alertService: AlertService,
+    private detailResultPackageService: DetailResultPackageService
   ) { }
 
   ngOnInit() {
     this.currentPackageId = +PackageDetailComponent.packageId;
   }
 
+  downloadTemplate(type) {
+    switch (type) {
+      case 'LostBid': {
+        this.detailResultPackageService.downloadTemplateLostBid().subscribe(response => {
+        },
+          err => {
+            this.alertService.error('Tải về template không thành công.');
+          });
+        break;
+      }
+      case 'WinBid': {
+        this.detailResultPackageService.downloadTemplateWinBid().subscribe(response => {
+        },
+          err => {
+            this.alertService.error('Tải về template không thành công.');
+          });
+        break;
+      }
+      case 'LessonLearn': {
+        this.detailResultPackageService.downloadTemplateLessonLearn().subscribe(response => {
+        },
+          err => {
+            this.alertService.error('Tải về template không thành công.');
+          });
+        break;
+      }
+    }
+  }
 }
