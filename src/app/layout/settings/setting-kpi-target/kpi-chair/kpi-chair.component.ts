693e402470619231fa56b24e8b528d7e25320899
quyendangbys <quyen.dang@bys.com.vn>
2019-02-13 14:26:41 +0700

    - UI popup thêm chủ trì vào nhóm

--- src/app/layout/settings/setting-kpi-target/kpi-chair/kpi-chair.component.ts
+++ src/app/layout/settings/setting-kpi-target/kpi-chair/kpi-chair.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Route, ActivatedRoute } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { BehaviorSubject } from 'rxjs';
 @Component({
   selector: 'app-kpi-chair',
   templateUrl: './kpi-chair.component.html',
@@ -9,15 +11,19 @@ import { Route, ActivatedRoute } from '@angular/router';
 export class KpiChairComponent implements OnInit {
   groupKpiChair: FormGroup;
   param: string;
+  modalRef: BsModalRef;
+  searchTerm$ = new BehaviorSubject<string>('');
   constructor(
     private fb: FormBuilder,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private modalService: BsModalService,
   ) { }
 
   ngOnInit() {
     this.activatedRoute.queryParams.subscribe(params => {
       this.param = params['action'];
     });
+    this.createForm();
   }
 
   createForm() {
@@ -25,6 +31,16 @@ export class KpiChairComponent implements OnInit {
     });
     // this.groupConfigForm.valueChanges
     //   .subscribe(data => this.onFormValueChanged(data));
+  }
+
+  addChairToGroupFuc(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, {
+      class: 'gray modal-lg'
+    });
+  }
+
+  closedPopup() {
+    this.modalRef.hide();
   }
 
 }
