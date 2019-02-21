4e5526f8eac2975357302add0a2cdb4cb0315fd5
quyendangbys <quyen.dang@bys.com.vn>
2019-02-21 17:58:38 +0700

    - report win rate quarter

--- src/app/layout/monitoring-report/report-win-rate-quarter/report-win-rate-quarter.component.ts
+++ src/app/layout/monitoring-report/report-win-rate-quarter/report-win-rate-quarter.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ReportFollowService } from '../../../shared/services/report-follow.service';
+import { ReportKpiWinRateQuarter } from '../../../shared/models/report-follow/report-kpi-win-rate-quarter.model';
+import { Subscription } from 'rxjs';
+import DateTimeConvertHelper from '../../../shared/helpers/datetime-convert-helper';
 
 @Component({
   selector: 'app-report-win-rate-quarter',
   templateUrl: './report-win-rate-quarter.component.html',
   styleUrls: ['./report-win-rate-quarter.component.scss']
 })
-export class ReportWinRateQuarterComponent implements OnInit {
-
-  constructor() { }
+export class ReportWinRateQuarterComponent implements OnInit, OnDestroy {
+  reportKpiWinRateQuarter: ReportKpiWinRateQuarter;
+  subscription: Subscription;
+  constructor(
+    private reportFollowService: ReportFollowService
+  ) { }
 
   ngOnInit() {
+    this.subscription = this.reportFollowService.startAndEndDate.subscribe(startAndEndDate => {
+      this.viewReport(startAndEndDate.startDate, startAndEndDate.endDate);
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  viewReport(startDate: Date, endDate: Date) {
+    const startDateNumber = DateTimeConvertHelper.fromDtObjectToTimestamp(startDate);
+    const endDateNumber = DateTimeConvertHelper.fromDtObjectToTimestamp(endDate);
+    // tslint:disable-next-line:max-line-length
+    this.reportFollowService.detailReportWinRateQuarter(startDateNumber, endDateNumber).subscribe(response => {
+      this.reportKpiWinRateQuarter = response;
+      console.log('reponse-reponse', response);
+    });
   }
 
 }
