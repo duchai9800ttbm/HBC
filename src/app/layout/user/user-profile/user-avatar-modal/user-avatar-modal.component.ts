1cc99db91935cc2add2ba9d843d5b0aba67ddac5
quyendangbys <quyen.dang@bys.com.vn>
2019-04-11 17:38:34 +0700

    - check type upload ảnh đại diện

--- src/app/layout/user/user-profile/user-avatar-modal/user-avatar-modal.component.ts
+++ src/app/layout/user/user-profile/user-avatar-modal/user-avatar-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { UserService, SessionService, AlertService } from '../../../../shared/services/index';
+import Utils from '../../../../shared/helpers/utils.helper';
 
 @Component({
   selector: 'app-user-avatar-modal',
@@ -10,7 +11,7 @@ import { UserService, SessionService, AlertService } from '../../../../shared/se
 export class UserAvatarModalComponent implements OnInit {
   imageChangedEvent: any = '';
   croppedImage: any = '';
-
+  errorMess: string;
   constructor(
     public activeModal: NgbActiveModal,
     private userService: UserService,
@@ -24,7 +25,13 @@ export class UserAvatarModalComponent implements OnInit {
 
 
   fileChangeEvent(event: any): void {
-    this.imageChangedEvent = event;
+    console.log('event.target.files;', event.target.files);
+    if (Utils.checkTypeFileImage(event.target.files)) {
+      this.errorMess = null;
+      this.imageChangedEvent = event;
+    } else {
+      this.errorMess = 'Hệ thống không hỗ trợ upload loại file này. Những loại file được hỗ trợ bao gồm jpg, .jpeg';
+    }
   }
   imageCropped(image: string) {
     this.croppedImage = image;
